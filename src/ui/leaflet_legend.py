38ca8e43e21d06ebcfcca37230be4a39fec85dda
dtomkatsu <141182668+dtomkatsu@users.noreply.github.com>
2025-10-01 13:12:43 -1000

    marginal improv

--- src/ui/leaflet_legend.py
+++ src/ui/leaflet_legend.py
@@ -14,63 +14,103 @@ const legendManager = {
         let labels = [];
         let grades;
         
-        if (SELECTED_VARIABLE.includes('poverty') || SELECTED_VARIABLE.includes('rate')) {
-            title += ' (%)';
-            grades = [[5, 10], [10, 15], [15, 20], [20, 25], [25, 30], [30, 35], [35, 40], [40, 50]];
-            
-            for (let i = 0; i < grades.length; i++) {
-                const from = grades[i][0];
-                const to = grades[i][1];
-                const isLast = i === grades.length - 1;
-                const range = isLast ? from + '%+' : from + '-' + to + '%';
-                
-                labels.push(
-                    '<div class="legend-item">' +
-                    '<i style="background:' + colors[i] + '"></i>' + range +
-                    '</div>'
-                );
-            }
-        } else if (SELECTED_VARIABLE.includes('income') || SELECTED_VARIABLE.includes('value')) {
-            title += ' ($)';
-            grades = [40, 50, 60, 70, 80, 90, 100, 110];
-            
+        // Match thresholds with getColorForValue function
+        if (SELECTED_VARIABLE === 'travel_time_to_work_minutes') {
+            title = 'Travel Time to Work (minutes)';
+            grades = [30, 35, 40, 45, 50];
             for (let i = 0; i < grades.length; i++) {
                 const from = grades[i];
                 const isLast = i === grades.length - 1;
-                const range = isLast ? from + 'k+' : from + '-' + grades[i+1] + 'k';
-                
-                labels.push(
-                    '<div class="legend-item">' +
-                    '<i style="background:' + colors[i] + '"></i>' + range +
-                    '</div>'
-                );
+                const range = isLast ? from + '+' : from + '-' + grades[i+1];
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE === 'public_transportation_pct') {
+            title = 'Public Transportation (%)';
+            grades = [0.5, 1.0, 2.0, 4.0, 6.0];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? from + '%+' : from + '-' + grades[i+1] + '%';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE === 'ctc_avg_amount' || SELECTED_VARIABLE === 'federal_eitc_avg_amount' || SELECTED_VARIABLE === 'state_eitc_avg_amount') {
+            title += ' ($)';
+            grades = [500, 750, 1000, 1250, 1500, 2000, 2500, 3000, 4000];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? '$' + (from/1000).toFixed(1) + 'k+' : '$' + (from/1000).toFixed(1) + 'k-' + (grades[i+1]/1000).toFixed(1) + 'k';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE === 'ctc_participation_rate' || SELECTED_VARIABLE === 'eitc_participation_rate') {
+            title += ' (%)';
+            grades = [5, 8, 10, 12, 15, 18, 20, 22, 25];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? from + '%+' : from + '-' + grades[i+1] + '%';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE === 'cep_percentage') {
+            title = 'Schools with CEP (%)';
+            grades = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? from + '%+' : from + '-' + grades[i+1] + '%';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE === 'cep_schools' || SELECTED_VARIABLE === 'total_schools' || SELECTED_VARIABLE === 'cep_display') {
+            title = 'Number of Schools';
+            grades = [5, 10, 15, 20, 25, 30, 40, 50, 60];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? from + '+' : from + '-' + grades[i+1];
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE === 'snap_benefits_annual_total') {
+            title = 'Total Annual SNAP Benefits ($)';
+            grades = [1000000, 2000000, 5000000, 10000000, 15000000, 20000000, 30000000, 50000000, 75000000];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? '$' + (from/1000000) + 'M+' : '$' + (from/1000000) + 'M-' + (grades[i+1]/1000000) + 'M';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE.includes('poverty') || SELECTED_VARIABLE.includes('rate')) {
+            title += ' (%)';
+            grades = [5, 10, 15, 20, 25, 30, 35, 40, 45];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? from + '%+' : from + '-' + grades[i+1] + '%';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
+            }
+        } else if (SELECTED_VARIABLE.includes('income')) {
+            title += ' ($)';
+            grades = [40000, 50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000];
+            for (let i = 0; i < grades.length; i++) {
+                const from = grades[i];
+                const isLast = i === grades.length - 1;
+                const range = isLast ? '$' + (from/1000) + 'k+' : '$' + (from/1000) + 'k-' + (grades[i+1]/1000) + 'k';
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
             }
         } else {
             title += ' (Count)';
-            grades = [
-                [0, 1], [1, 5], [5, 10], [10, 25], 
-                [25, 50], [50, 100], [100, 250], [250, 500]
-            ];
-            
+            grades = [0, 1000, 5000, 10000, 25000, 50000, 100000, 250000, 500000];
             for (let i = 0; i < grades.length; i++) {
-                const from = grades[i][0];
-                const to = grades[i][1];
+                const from = grades[i];
                 const isLast = i === grades.length - 1;
                 let range;
-                
                 if (isLast) {
-                    range = from + 'k+';
-                } else if (to >= 1) {
-                    range = from + 'k-' + to + 'k';
+                    range = (from/1000) + 'k+';
+                } else if (from >= 1000) {
+                    range = (from/1000) + 'k-' + (grades[i+1]/1000) + 'k';
                 } else {
-                    range = from + '-' + to;
+                    range = from + '-' + grades[i+1];
                 }
-                
-                labels.push(
-                    '<div class="legend-item">' +
-                    '<i style="background:' + colors[i] + '"></i>' + range +
-                    '</div>'
-                );
+                labels.push('<div class="legend-item"><i style="background:' + colors[i] + '"></i>' + range + '</div>');
             }
         }
         
